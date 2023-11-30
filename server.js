5d40e22c804f835eb3e215ec4601a89ac3ce5a0e
Daichi <7322083@ed.tus.ac.jp>
2023-11-30 13:51:52 +0900

    PORT

--- server.js
+++ server.js
@@ -19,6 +19,6 @@ io.on("connection", (socket) => {
     });
 });
 
-server.listen(PORT, () => {
-    console.log("listening on 3000");
+server.listen(process.env.PORT || PORT, () => {
+    console.log("server running...");
 });
